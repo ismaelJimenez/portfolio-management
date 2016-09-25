50aa0d67f69f2a49a5321731abc6670d0a0f0160
ismael <ijimenezm@hotmail.es>
2016-09-25 18:35:52 +0200

    added first portfolio managment lib

---
+++ libraries/portfolio_optimization.r
@@ -0,0 +1,29 @@
+# portfolio.r
+# 
+# Functions for portfolio analysis
+# last updated: September 25, 2016 by Ismael Jimenez
+#
+# Functions:
+#	1. efficient.portfolio.risk			compute minimum variance portfolio
+#							                    subject to target risk
+
+stopifnot("package:quadprog" %in% search()  ||  require("quadprog",quietly = TRUE) )
+
+source("http://faculty.washington.edu/ezivot/econ424/portfolio_noshorts.r")
+
+efficient.portfolio.risk <-
+  function(er, cov.mat, target.risk, shorts=TRUE)
+{
+  incr <- 0.0001
+  e.port <- globalMin.portfolio(er, cov.mat, shorts)
+  currentEr <- as.numeric(e.port["er"])
+  currentSd <- as.numeric(e.port["sd"])
+  
+  while(currentSd < target.risk) {
+    currentEr <- currentEr+incr
+    e.port <- efficient.portfolio(er, covmat, currentEr, shorts)
+    currentEr <- as.numeric(e.port["er"])
+    currentSd <- as.numeric(e.port["sd"])
+  }
+  e.port
+}
